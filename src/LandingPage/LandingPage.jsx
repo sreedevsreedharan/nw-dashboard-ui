73a620e5b7ee2d455ff1005426193fe756dc17f2
sreedevsreedharan <66953953+sreedevsreedharan@users.noreply.github.com>
2022-07-06 16:05:18 +0530

    Adding react router

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from "react";
 import Home from "../Home/Home";
 import SidePanel from "../SidePanel/SidePanel";
 import TrackVacation from "../TrackVacation/TrackVacation";
@@ -6,27 +7,21 @@ import './LandingPage.scss';
 import initialLoad from '../temp/initialLoad.json';
 import { useDispatch } from "react-redux";
 import { addLeaveToday, addUsers, addVacationPending } from "../common/redux/DashBoardSlice";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const LandingPage = () => {
 
     const dispatch = useDispatch();
+
     
-
-    const[currentScreen, setCurrentScreen] = useState(true);
-
-    useEffect(()=>{
-        console.log('initialLoad',initialLoad);
+    useEffect(() => {
         dispatch(addUsers(initialLoad.allUsers));
         dispatch(addLeaveToday(initialLoad.currentDayLeave));
         dispatch(addVacationPending(initialLoad.pendingVacationUser));
+    }, []);
 
-    },[]);
 
-    const toggleScreen = () =>{
-        setCurrentScreen(!currentScreen);
-    }
-
-    return(
+    return (
         <div className="row landing-page-parent">
             <div className="col-md-12">
                 <div className="row">
@@ -34,16 +29,22 @@ const LandingPage = () => {
                         NW Dashboard
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-md-2 side-panel-parent">
-                        <SidePanel toggleScreen={toggleScreen}/>
+                <BrowserRouter>
+                    <div className="row">
+                        <div className="col-md-2 side-panel-parent">
+                            <SidePanel/>
+                        </div>
+                        <div className="col-md-10">
+
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/trackVacation" element={<TrackVacation />} />
+                            </Routes>
+
+
+                        </div>
                     </div>
-                    <div className="col-md-10">
-                        {currentScreen && <Home />}
-                        {!currentScreen && <TrackVacation />}
-                        
-                    </div>
-                </div>
+                </BrowserRouter>
             </div>
         </div>
     )
