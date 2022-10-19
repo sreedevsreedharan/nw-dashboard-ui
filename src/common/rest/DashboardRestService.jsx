b9a5bf37ac8d956b2fccbdc92c29b05b0af54207
sreedevsreedharan <66953953+sreedevsreedharan@users.noreply.github.com>
2022-10-19 16:45:49 +0530

    heroku push

--- src/common/rest/DashboardRestService.jsx
+++ src/common/rest/DashboardRestService.jsx
@@ -4,27 +4,27 @@ import axios from "axios";
 export default class DashboardRestService {
 
     getOnLoadData = () => {
-        return axios.get('http://localhost:8080/dashboard/v1/load');
+        return axios.get('https://nw-dashboard-service-app.herokuapp.com/dashboard/v1/load');
     }
 
     getVacations = (gpn) => {
-        return axios.get(`http://localhost:8080/dashboard/v1/${gpn}/vacations`);
+        return axios.get(`https://nw-dashboard-service-app.herokuapp.com/dashboard/v1/${gpn}/vacations`);
     }
 
     saveVacations = (saveObject) => {
-        return axios.post(`http://localhost:8080/dashboard/v1/add/vacations`,saveObject);
+        return axios.post(`https://nw-dashboard-service-app.herokuapp.com/dashboard/v1/add/vacations`,saveObject);
     }
 
     getReport = () => {
-        return axios.get('http://localhost:8080/dashboard/v1/fetch-report');
+        return axios.get('https://nw-dashboard-service-app.herokuapp.com/dashboard/v1/fetch-report');
     }
 
     getProjects = () => {
-        return axios.get('http://localhost:8080/dashboard/v1/fetch-projects');
+        return axios.get('https://nw-dashboard-service-app.herokuapp.com/dashboard/v1/fetch-projects');
     }
 
     saveUser = (saveObject) => {
-        return axios.post('http://localhost:8081/dashboard/v1/add/user',saveObject);
+        return axios.post('https://nw-dashboard-service-app.herokuapp.com/dashboard/v1/add/user',saveObject);
     }
 
 }
