8fa027bb774efacacbaf15ecb7abb5bf48aba343
sreedevsreedharan <66953953+sreedevsreedharan@users.noreply.github.com>
2022-11-04 10:37:52 +0530

    Side panel css change to show active link

--- src/SidePanel/SidePanel.jsx
+++ src/SidePanel/SidePanel.jsx
@@ -1,38 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { addUsers, defaulterMail, editUsers, home, trackVacation, viewReport } from "../common/constants/constants";
 import './SidePanel.scss';
 
 const SidePanel = ({ defaultersFeature }) => {
+
+    const getCurrentLocation = () => {
+        const location = window.location.pathname;
+        switch (location) {
+            case "/":
+                return "Home";
+            case "/trackVacation":
+                return "Track";
+            case "/viewReport":
+                return "View";
+            case "/defaulterMail":
+                return "Defaulter";
+            case "/addUsers":
+                return "AddUser";
+            default:
+                break;
+        }
+    }
+
+    const [active, setActive] = useState(getCurrentLocation());
+    const getClassName = (link) => {
+        if (active === link) {
+            return "text-light active-link";
+        } else {
+            return "text-light";
+        }
+    }
+
     return (
         <div className="row mt-3 ms-3">
             <div className="col-md-12">
                 <div className="row mt-3">
                     <div className="col-md-12">
-                        <Link className="text-light" to="/">{home}</Link>
+                        <Link onClick={() => setActive('Home')} className={getClassName('Home')} to="/">{home}</Link>
                     </div>
                 </div>
                 <div className="row mt-3">
                     <div className="col-md-12">
-                        <Link className="text-light" to="/trackVacation">{trackVacation}</Link>
+                        <Link onClick={() => setActive('Track')} className={getClassName('Track')} to="/trackVacation">{trackVacation}</Link>
 
                     </div>
                 </div>
                 <div className="row mt-3">
                     <div className="col-md-12">
-                        <Link className="text-light" to="/viewReport">{viewReport}</Link>
+                        <Link onClick={() => setActive('View')} className={getClassName('View')} to="/viewReport">{viewReport}</Link>
                     </div>
                 </div>
                 {defaultersFeature === "true" && <div className="row mt-3">
                     <div className="col-md-12">
-                        <Link className="text-light" to="/defaulterMail">{defaulterMail}</Link>
+                        <Link onClick={() => setActive('Defaulter')} className={getClassName('Defaulter')} to="/defaulterMail">{defaulterMail}</Link>
                     </div>
                 </div>}
-                {/* <div className="row mt-3">
+                <div className="row mt-3">
                     <div className="col-md-12">
-                        <Link className="text-light" to="/addUsers">{addUsers}</Link>
+                        <Link onClick={() => setActive('AddUser')} className={getClassName('AddUser')} to="/addUsers">{addUsers}</Link>
                     </div>
                 </div>
+                {/*
                 <div className="row mt-3">
                     <div className="col-md-12">
                         <Link className="text-light" to="/editUsers">{editUsers}</Link>
